94d2262369baa5ca3072e4fe53eb38348d88bff8
petrsmrz <petr.smrz@gmail.com>
2024-06-26 16:40:20 +0200

    Stream

--- src/App_old.js
+++ src/App_old.js
@@ -1,36 +1,36 @@
 import "./App.css";
-import React, { useRef, useState } from "react";
+import React from "react";
+import { useRef, useState } from "react";
 
 function App() {
+  //const [number, setNumber] = useState(0);
   const [syDecodedString, setSyDecodedString] = useState("");
   const [dtDecodedString, setDtDecodedString] = useState("");
   const [mtDecodedString, setMtDecodedString] = useState("");
 
   const inputRef = useRef(null);
+  // QLR service and characteristics
+  const QLR_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+  const QLR_SUB_WRITE = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"; // write
+  const QLR_SUB_NOFIT = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"; // notify
+  // Sylvac service and characteristics
+  const SY_SERVICE = "c1b25000-caaf-6d0e-4c33-7dae30052840";
+  const SY_SUB_INDIC = "c1b25010-caaf-6d0e-4c33-7dae30052840";
+  const SY_SUB_NOTIF = "c1b25013-caaf-6d0e-4c33-7dae30052840";
+  const SY_SUB_CMD = "c1b25012-caaf-6d0e-4c33-7dae30052840";
 
-  // Service and characteristics UUIDs
-  const SERVICES = {
-    QLR: {
-      service: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
-      write: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
-      notify: "6e400003-b5a3-f393-e0a9-e50e24dcca9e",
-    },
-    SY: {
-      service: "c1b25000-caaf-6d0e-4c33-7dae30052840",
-      indic: "c1b25010-caaf-6d0e-4c33-7dae30052840",
-      notify: "c1b25013-caaf-6d0e-4c33-7dae30052840",
-      cmd: "c1b25012-caaf-6d0e-4c33-7dae30052840",
-    },
-    MT: {
-      service: "7eafd361-f150-4785-b307-47d34ed52c3c",
-      char: "7eafd361-f151-4785-b307-47d34ed52c3c",
-      trigger: "7eafd361-f155-4785-b307-47d34ed52c3c",
-    },
-    WERKA: {
-      service: "00035b03-58e6-07dd-021a-08123a000300",
-      write: "00035b03-58e6-07dd-021a-08123a0003ff",
-      indic: "00035b03-58e6-07dd-021a-08123a000301",
-    },
+  // Mitutoyo service and characteristics
+  const MT_SERVICE = "7eafd361-f150-4785-b307-47d34ed52c3c";
+  const MT_SUB_CHAR = "7eafd361-f151-4785-b307-47d34ed52c3c";
+  const MT_TRI_CHAR = "7eafd361-f155-4785-b307-47d34ed52c3c";
+
+  // Werka new indicator BLE5
+  const WERKA_SERVICE = "00035b03-58e6-07dd-021a-08123a000300";
+  const WERKA_SUB_WRITE = "00035b03-58e6-07dd-021a-08123a0003ff"; // read write
+  const WERKA_SUB_INDIC = "00035b03-58e6-07dd-021a-08123a000301"; // read write indicate
+
+  const extractCharacters = (name, startIndex, endIndex) => {
+    return name.substring(startIndex, endIndex);
   };
   function formatToOriginalDecimals(inputString) {
     // Parse the input string as a float
@@ -50,79 +50,110 @@ function App() {
 
   const bleConnectOptions = {
     filters: [
-      { services: [SERVICES.QLR.service] },
-      { services: [SERVICES.SY.service] },
-      { services: [SERVICES.WERKA.service] },
-      { services: [SERVICES.MT.service] },
+      { services: [QLR_SERVICE] },
+      { services: [SY_SERVICE] },
+      { services: [WERKA_SERVICE] },
+      { services: [MT_SERVICE] },
+      { name: extractCharacters("014333038", 0, 4) },
+      { name: "UWAVE" },
       { namePrefix: "SY" },
     ],
   };
-
   console.log("BLE Connect Options", bleConnectOptions);
 
   const connectDevice = async () => {
     try {
+      //show drop down list with devices to connect
       const device = await navigator.bluetooth.requestDevice(bleConnectOptions);
-      console.log("Device", device);
+      console.log("device", device);
+      console.log("device name", device.name);
+      const deviceName = device.name;
 
+      //connect gatt server within the device
+      console.log("connceting gatt server");
       const server = await device.gatt.connect();
-      console.log("GATT Server connected", server);
+      console.log("gatt server connected", server);
 
-      let service = null;
-      let characteristics = null;
+      // choose setvice as per manufacturer config
+      const useService = device.name.startsWith("SY")
+        ? SY_SERVICE
+        : device.name.startsWith("D")
+        ? WERKA_SERVICE
+        : device.name.startsWith(deviceName)
+        ? MT_SERVICE
+        : QLR_SERVICE;
 
-      for (const key in SERVICES) {
-        try {
-          service = await server.getPrimaryService(SERVICES[key].service);
-          console.log(`Connected to ${key} service:`, service);
-          characteristics = SERVICES[key];
-          break;
-        } catch (error) {
-          console.log(
-            `Service ${SERVICES[key].service} not found on this device.`
-          );
-        }
-      }
+      const useNotifChar = device.name.startsWith("SY")
+        ? SY_SUB_NOTIF
+        : device.name.startsWith("D")
+        ? WERKA_SUB_INDIC
+        : device.name.startsWith(deviceName)
+        ? MT_SUB_CHAR
+        : QLR_SUB_NOFIT;
+      console.log("Selected Notification Char =", useNotifChar);
 
-      if (!service) {
-        throw new Error("No known service found on this device.");
-      }
+      const useIndChar = device.name.startsWith("SY")
+        ? SY_SUB_INDIC
+        : device.name.startsWith("D")
+        ? WERKA_SUB_INDIC
+        : device.name.startsWith(deviceName)
+        ? MT_SUB_CHAR
+        : QLR_SUB_NOFIT;
+      console.log("Selected Indication Char =", useIndChar);
 
-      const notifyCharacteristic = await service.getCharacteristic(
-        characteristics.notify || characteristics.char
+      const useCmdChar = device.name.startsWith("SY")
+        ? SY_SUB_CMD
+        : device.name.startsWith("D")
+        ? WERKA_SUB_WRITE
+        : device.name.startsWith(deviceName)
+        ? MT_TRI_CHAR
+        : QLR_SUB_WRITE;
+
+      //read ble services witihin the device
+      console.log("connecting services ...");
+      const service = await server.getPrimaryService(useService);
+      console.log("services", service);
+
+      // read ble characteristic within the device
+      const notifCharacteristics = await service.getCharacteristic(
+        useNotifChar
       );
-      await notifyCharacteristic.startNotifications();
-      notifyCharacteristic.addEventListener(
+      console.log(
+        "Notification characteristic " + JSON.stringify(notifCharacteristics)
+      );
+      await notifCharacteristics.startNotifications();
+      console.log("Notification characteristics notifications started");
+      notifCharacteristics.addEventListener(
         "characteristicvaluechanged",
         handleInd
       );
 
-      if (characteristics.indic) {
-        const indicCharacteristic = await service.getCharacteristic(
-          characteristics.indic
-        );
-        await indicCharacteristic.startNotifications();
-        indicCharacteristic.addEventListener(
-          "characteristicvaluechanged",
-          handleInd
-        );
-      }
-
-      window.cmdCharacteristic = await service.getCharacteristic(
-        characteristics.cmd || characteristics.write
+      const indicCharacteristic = await service.getCharacteristic(useIndChar);
+      console.log(
+        "Indication characteristic " + JSON.stringify(indicCharacteristic)
       );
+      await indicCharacteristic.startNotifications();
+      console.log("Idication characteristics notifications started");
+      indicCharacteristic.addEventListener(
+        "characteristicvaluechanged",
+        handleInd
+      );
+      window.cmdCharacteristic = await service.getCharacteristic(useCmdChar);
+      console.log("CMD characteristic", window.cmdCharacteristic);
       window.cmdCharacteristic.addEventListener("submit", writeCommand, false);
+      /*  let enc = new TextEncoder("utf-8");
+      indicCharacteristic.writeValue(enc.encode("?\r"));
+      window.btch = indicCharacteristic;*/
 
       function handleInd(event) {
         let value = event.target.value;
         console.log("Value =", value);
-
-        if (characteristics === SERVICES.SY) {
-          const syDecoder = new TextDecoder("utf-8");
+        if (device.name.startsWith("SY") || device.name.startsWith("Q")) {
+          const syDecoder = new TextDecoder("utf-8"); // Sylvac decoder
           const syDecodedValue = syDecoder.decode(value);
           setSyDecodedString(syDecodedValue);
           console.log("Read SY value", syDecodedValue);
-        } else if (characteristics === SERVICES.MT) {
+        } else if (device.name.startsWith(deviceName)) {
           const mtDecodedValue = mtDecoder(value);
           setMtDecodedString(mtDecodedValue);
           console.log("Read MT value", mtDecodedValue);
@@ -132,7 +163,7 @@ function App() {
           console.log("Read DT value", dtDecodedValue);
         }
       }
-
+      // Mitutoyo decoder
       function mtDecoder(val) {
         console.log("mtDecoder echo Read value ", val);
         const dataView = new DataView(val.buffer);
@@ -141,13 +172,18 @@ function App() {
         const mtDecodedValue = uwaveVal.toString().trim();
         return mtDecodedValue;
       }
-
+      // Werka decoder
       function dtDecoder(val) {
         console.log("dtDecoder val", val);
+
         const sliced = [...new Uint8Array(val.buffer)];
         console.log("dtDecoder sliced", sliced);
+
         const v = buf2hex(val.buffer);
+
         console.log("dtDecoder v", v);
+
+        //const v1 = v.slice(-7, -2).replace(/^0+/, '') + '.' + v.slice(-2);
         let v1 = v.slice(4);
         const isNegative = isKthBitSet(sliced[1], 7);
         const isInches = isKthBitSet(sliced[1], 6);
@@ -158,6 +194,7 @@ function App() {
           "." +
           v1.slice(-decimals);
         console.log("dtDecoder v1", v1, isNegative, isInches, decimals);
+        // console.log('v1 ' + v1)
         const decodedValue = Intl.NumberFormat("en-GB", {
           signDisplay: "always",
           maximumSignificantDigits: 5,
@@ -167,41 +204,44 @@ function App() {
       }
 
       function buf2hex(buffer) {
+        // buffer is an ArrayBuffer
         return [...new Uint8Array(buffer)]
           .map((x) => x.toString(16).padStart(2, "0"))
           .join("");
       }
 
       function isKthBitSet(n, k) {
-        return (n & (1 << k)) > 0;
+        if ((n & (1 << k)) > 0) return true;
+        else return false;
       }
 
       function kthBit(n, k) {
-        return (n & (1 << k)) > 0 ? 1 : 0;
+        if ((n & (1 << k)) > 0) return 1;
+        else return 0;
       }
     } catch (err) {
-      console.error("Error", err);
+      console.error("error", err);
     }
   };
-
+  /* function handleInd(event) {
+    let value = event.target.value;
+    const decoder = new TextDecoder("utf-8");
+    const decodedValue = decoder.decode(value);
+    setSyDecodedString(decodedValue);
+    console.log("Read value", decodedValue);
+  }
+    */
   const handleClick = () => {
     const value = inputRef.current.value;
     console.log("handleClick", value);
     writeCommand(value);
   };
-  const handleClickCmd = () => {
-    const value = "OUT1";
-    console.log("handleClick", value);
-    writeCommand(value);
-  };
-
   const enc = new TextEncoder("utf-8");
 
   function writeCommand(cmd) {
     console.log("Command received", enc.encode(cmd + "\r"));
     window.cmdCharacteristic.writeValue(enc.encode(cmd + "\r"));
   }
-
   return (
     <div className="App">
       <button
@@ -244,7 +284,6 @@ function App() {
         >
           <h1 style={{ padding: "0px", margin: "5px" }}>Submit</h1>
         </button>
-        <button onClick={handleClickCmd}>Stream</button>
       </div>
     </div>
   );
